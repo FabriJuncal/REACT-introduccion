f29470c311a042750671962c2c66d3101c56e351
fjuncal <fjuncal@tdi.com.ar>
2021-01-21 19:40:47 -0300

    38. Creando nuestro primer Componente

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
+// Importamos el componente
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hola Mundo</h1>
+       {/* Utilizamos el componente creado */}
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+// Creamos el componente, por lo general es una funcion con un return que retorna etiquetas.
+function Header(){
+
+    return(
+        <h1>Desde el Header</h1>
+    )
+}
+
+// Una vez creado el componente lo exportamos con la siguiente sintaxis
+export default Header;
