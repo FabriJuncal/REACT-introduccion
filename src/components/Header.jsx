79f06b38c74ff5cf8239fc5fb79b24663cfc3e11
fjuncal <fjuncal@tdi.com.ar>
2021-01-21 20:28:14 -0300

    39. Básicos de JSX

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,30 @@
 // Creamos el componente, por lo general es una funcion con un return que retorna etiquetas.
 function Header(){
 
+    // // 1) Logica antes de retornar las etiquetas
+    // const edad = 18
+
+    // let mensaje;
+    // if(edad  >= 18){
+    //     mensaje = "Eres mayor de edad";
+    // }else{
+    //     mensaje = "Eres menor de edad";
+    // }
+
+    // // Para imprimir variables JS dentro del return, utilizar corchetes "{}"
+    // return(
+    //     <h1>{mensaje}</h1>
+    // )
+
+// --------------------------------------------------------------------------------------------------------------
+    
+    // 2) Asignar Clases e ID's a las etiquetas dentro del "return"
+    // Clases: Para asignar clases a etiquetas dentro del "return", deberán ir con el nombre "className",
+    //         por que "class" es una palabra reservada de JavaScript.
+    // ID's: Se asignan de manera igual a como se hace en HTML.
+
     return(
-        <h1>Desde el Header</h1>
+        <h1 id="encabezado" className="encabezado">Tienda Virtual</h1>
     )
 }
 
