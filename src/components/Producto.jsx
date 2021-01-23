a50773242a71ff4445f2f3fdfce6f89570ef5153
fjuncal <fjuncal@tdi.com.ar>
2021-01-23 19:41:27 -0300

    45. Eventos en React

--- src/components/Producto.jsx
+++ src/components/Producto.jsx
@@ -6,10 +6,21 @@ const Producto = ({producto}) => {
     // Utilizamos sintaxis llamada "Destructuring" para definir una variable con el valor del array/objeto al mismo tiempo.
     const {id, nombre, precio} = producto;
 
+    // Creamos una función que sera asignado al evento "onClick" del botón
+    const seleccionarProducto = id => {
+        console.log("Comprando...", id);
+    }
+
     return ( 
         <div>
             <h2>{nombre}</h2>
             <p>${precio}</p>
+            {/* De este modo asignamos una función a un evento,
+                Si no utilizaramos el "arrow function", la funcíon no esperaría el evento del componente y se ejecutaría al cargar la pagina */}
+            <button
+                type="button"
+                onClick= { () => seleccionarProducto(id) }
+            >Comprar</button>
         </div>
      );
 }
