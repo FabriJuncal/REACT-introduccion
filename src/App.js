00c44e104ce4b914e60e3a9aed71ec91334b7e1b
fjuncal <fjuncal@tdi.com.ar>
2021-01-23 18:37:29 -0300

    43. Primeros pasos con el State de React

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 // Importamos la libreria "Fragment" que viene con React para poder utilizar los beneficios
 // de la etiqueta/componente <Fragment></Fragment>
-import React, {Fragment} from "react";
+import React, {Fragment, useState} from "react";
 // Importamos el componente
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 function App() {
 
-// Cremos variables o condicionales en este espacio
-
+// Creamos variables o condicionales en este espacio
 const fecha = new Date().getFullYear();
+
+// Creamos listado de productos utilizando la función "useStates"
+const [productos, guardarProductos] = useState([
+  { id: 1, nombre: 'Camisa ReactJS', precio: 50},
+  { id: 2, nombre: 'Camisa VueJS', precio: 50},
+  { id: 3, nombre: 'Camisa Node.js', precio: 50},
+  { id: 4, nombre: 'Camisa Angular', precio: 50},
+]);
 
 // ============================================================================================================================================
 
